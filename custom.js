e72bae0f2dcde2789f8fe2ab358b7d92d74ff9ae
davutozturk <61davut14@gmail.com>
2022-04-05 17:39:49 +0300

    eklemeler yapıldı

--- custom.js
+++ custom.js
@@ -10,5 +10,44 @@ $(document).ready(function () {
     $("#Show-Hide").click(function () {
     writings.toggle(600);
     });
+    $("#Open-Close").click(function () {
+    writings.slideUp(800).slideDown(400);
+    });
+    $("#Add-Element").click(function () {
+    writings.append("Object");
+    writings.prepend("Subject");
+    });
+    $("#Delete-Element").click(function () {
+    $("li:first-child").remove();
+    });
+    $("#Add-Class").click(function () {
+    writings.addClass("add");
+    });
+    $("#Change-Class").click(function () {
+    writings.toggleClass("add");
+    });
+
+    var box1 = $(".box1");
+    var box2 = $(".box2");
+    var pn1 = box1.position();
+    var lft1 = pn1.left;
+    var pn2 = box2.position();
+    var lft2 = pn2.left;
+    $("#Start").click(function () {
+    box1.css({ position: "relative" });
+    while (lft1 <= 600) {
+        box1.animate({
+            left: "+=30",
+        });
+        lft1 += 30;
+    }
+    box2.css({ position: "relative" });
+    while (lft2 <= 800) {
+        box2.animate({
+            left: "+=40",
+        });
+        lft2 += 40;
+    }
+    });
 
 });
