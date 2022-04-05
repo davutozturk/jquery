5c6877cf0368660923d044556652d4d157b12410
davutozturk <61davut14@gmail.com>
2022-04-05 15:29:43 +0300

    eklemeler yapıldı

---
+++ custom.js
@@ -0,0 +1,12 @@
+$(document).ready(function () {
+
+    $("#Hide").click(function () {
+    console.log("okay...");
+    $(".article").hide(800);
+    });
+    $("#Show").click(function () {
+    console.log("okay...");
+    $(".article").show(400);
+    });
+
+});
